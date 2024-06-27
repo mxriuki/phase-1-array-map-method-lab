1a7f85be3913c39fef2c48cb4980483c6da04e7a
mxriuki <mwendav37@gmail.com>
2024-06-27 09:56:15 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 const titleCased = () => {
   return tutorials
 }
+    return tutorials.map(tutorial => {
+      let words=tutorial.split(' ')
+      let capitalizedWords=words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      let result=capitalizedWords.join(' ')
+      return result
+    })
+  }
+
+
+
